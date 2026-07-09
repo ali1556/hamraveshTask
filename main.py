4894c33178d73845d1f8504f4280bb63c5ff89a3
ali1556 <ali.nouri@g2holding.org>
2026-07-09 16:05:59 +0330

    added regex + implementing some of the feautures

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 import re
 
-file = open("access.log", "r")
-
-# content = file.read()
-# print(content)
-number_of_lines_read = 0
-for line in file:
-    parts = line.split()
-    for part in parts:
-        print(part)
-    print(len(parts))
-    number_of_lines_read += 1
-    if number_of_lines_read >= 2:
-        break
-file.close()
-
-
-
 invalid_line_count = 0
+valid_count = 0
 total_number_of_requests = 0
 unique_ip_addresses = set()
 addresses_with_most_traffic = {}
 error_rate = 0
+number_of_errors = 0
 traffic_per_hour = {}
 
-def read_file (file): 
-    for line in file:
-        line.strip()
-        total_number_of_requests += 1
-        parts = line.split()
-        if len(parts) < 9:
-            invalid_line_count += 1
-            continue
-        ip_address = parts[0]
-        unique_ip_addresses.add(ip_address)
-        status_code = parts[8]
-        if status_code.startswith('4') or status_code.startswith('5'):
-            error_rate += 1
-        timestamp = parts[3][1:12]  
-        if timestamp not in traffic_per_hour:
-            traffic_per_hour[timestamp] = 0
-        traffic_per_hour[timestamp] += 1
-        if ip_address not in addresses_with_most_traffic:
-            addresses_with_most_traffic[ip_address] = 0
-        addresses_with_most_traffic[ip_address] += 1
+
+log_pattern = re.compile(
+    r'^(?P<ip>\d+\.\d+\.\d+\.\d+) - - \[(?P<timestamp>[^\]]+)\] "(?P<method>[A-Z]+) (?P<path>[^"]+) HTTP/\d\.\d" (?P<status>\d{3}) (?P<bytes>\d+) "(?P<referrer>[^"]*)" "(?P<user_agent>[^"]*)"$'
+)
+
+def is_valid_log(line):
+    line = line.strip()
+    if not line :
+        return False, None, "Empty line"
+    match = log_pattern.match(line)
+    if not match:
+        return False, None, "Invalid log format"
+    data = match.groupdict()
+    return True, data, None
+
+def endpoint_handle(endpoint):
+    if endpoint in addresses_with_most_traffic:
+        addresses_with_most_traffic[endpoint] += 1
+    else:
+        addresses_with_most_traffic[endpoint] = 1
+
+file = open("access.log", "r")
+
+for i, line in enumerate(file):
+    if i>=1000:
+        break
+    is_valid, data, error = is_valid_log(line)
+    total_number_of_requests += 1
+    if is_valid: 
+        valid_count += 1
+        unique_ip_addresses.add(data['ip'])
+        endpoint_handle(data['path'])
+        number_of_errors += 1 if data['status'].startswith('4') or data['status'].startswith('5') else 0
+
+    else:
+        invalid_line_count += 1
+        print(f"invalid entry number : {i}")
+        print(f"Invalid entry: {error}")
+        print(f"Line: {line.strip()}")
+
+file.close()
+
+
     
